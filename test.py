40ac5ef40d3004d6d7c8cf998809691d5c91a080
root <georgeongit@gmail.com>
2024-11-15 08:33:57 +0000

    add requirements.txt

--- test.py
+++
@@ -1,9 +0,0 @@
-import asyncio, aiohttp
-from Tools.utils import round_value, get_wind_direction
-
-
-async def main():
-    temperature = await round_value(value=12.22553)
-
-if __name__ == "__main__":
-    asyncio.run(main())
